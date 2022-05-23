7c127c9ff8bdd69932ccff669f6d4ecb30e4a66a
ironBoni <noam60@gmail.com>
2022-05-23 19:56:39 +0300

    updated contact

--- android/app/src/main/java/com/ex3/androidchat/MainActivity.java
+++ android/app/src/main/java/com/ex3/androidchat/MainActivity.java
@@ -1,14 +1,51 @@
 package com.ex3.androidchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.ex3.androidchat.adapters.FragAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewPager pager;
+    TabLayout tabMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*pager = findViewById(R.id.pager);
+        pager.setAdapter(new FragAdapter(getSupportFragmentManager()));
+
+        tabMenu = findViewById(R.id.tabMenu);
+        tabMenu.setupWithViewPager(pager);*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inf =getMenuInflater();
+        inf.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.settings) {
+            // settings
+        } else {
+            // logout
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ android/app/src/main/java/com/ex3/androidchat/adapters/FragAdapter.java
@@ -0,0 +1,35 @@
+package com.ex3.androidchat.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.ex3.androidchat.fragments.ChatFragment;
+
+public class FragAdapter extends FragmentPagerAdapter {
+    public FragAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        // if(position == 0)
+        return new ChatFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String head = "Chat";
+        //(if position == 0) head = chat
+        return head;
+    }
+}
