4eb88aa2a68f06bebd7ee23f476cf73a74de199b
hadarpinto <hadarpinto@gmail.com>
2022-05-31 16:10:04 +0300

    fix main activity

---
+++ android/app/src/main/java/com/ex3/androidchat/AddUserActivity.java
@@ -0,0 +1,14 @@
+package com.ex3.androidchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
