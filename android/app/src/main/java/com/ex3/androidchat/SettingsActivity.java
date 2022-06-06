21df4851f5f98a1cea0d3bb579c847c7afac84da
ironBoni <noam60@gmail.com>
2022-06-06 18:48:00 +0300

    after entering a message it updates the recyclerView

--- android/app/src/main/java/com/ex3/androidchat/SettingsActivity.java
+++ android/app/src/main/java/com/ex3/androidchat/SettingsActivity.java
@@ -2,16 +2,19 @@ package com.ex3.androidchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.ex3.androidchat.services.UserService;
 
 public class SettingsActivity extends AppCompatActivity {
     Button btnSaveChanges;
+    ImageView btnBack;
     EditText txtSettingsServer;
     UserService userService;
 
@@ -22,6 +25,16 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.settings);
 
         userService = new UserService();
+
+        btnBack = findViewById(R.id.btnBackSettings);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         btnSaveChanges = findViewById(R.id.btnSaveSettings);
         btnSaveChanges.setOnClickListener(new View.OnClickListener() {
             @Override
