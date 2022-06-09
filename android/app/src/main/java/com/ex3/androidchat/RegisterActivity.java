81c66ad5aa96549b695044690c8be79cf2d1eabd
ironBoni <noam60@gmail.com>
2022-06-09 23:45:43 +0300

    image saved in server DB after user registers

--- android/app/src/main/java/com/ex3/androidchat/RegisterActivity.java
+++ android/app/src/main/java/com/ex3/androidchat/RegisterActivity.java
@@ -3,11 +3,11 @@ package com.ex3.androidchat;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -94,12 +94,14 @@ public class RegisterActivity extends AppCompatActivity {
                 txtNickname = findViewById(R.id.txtNickname);
                 txtConfirm = findViewById(R.id.txtConfirm);
 
-                BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-                Bitmap bitmap = drawable.getBitmap();
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
-                byte[] byteArray = byteArrayOutputStream.toByteArray();
-                String imageBytesStr = String.valueOf(byteArray);
+                imageView.setDrawingCacheEnabled(true);
+                imageView.buildDrawingCache();
+                Bitmap bitmap = Bitmap.createBitmap(imageView.getDrawingCache());
+
+                ByteArrayOutputStream byteArrayOutputStream = new  ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG,100, byteArrayOutputStream);
+                byte[] b=byteArrayOutputStream.toByteArray();
+                String imageBytesStr = Base64.encodeToString(b, Base64.DEFAULT);
 
                 User user = new User(txtUserIdR.getText().toString(),
                         txtNickname.getText().toString(), txtPasswordR.getText().toString(), imageBytesStr);
