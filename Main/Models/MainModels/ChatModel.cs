5be028435a376e27e3d98ed47f6f5bc95d4f4af3
ironBoni <noam60@gmail.com>
2022-06-09 19:36:46 +0300

    changes in the models

---
+++ Main/Models/MainModels/ChatModel.cs
@@ -0,0 +1,35 @@
+﻿using Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspWebApi.Models.MainModels {
+    public class ChatModel {
+        private static int id = 16;
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the participants.")]
+        public List<string> Participants { get; set; }
+
+        [Required(ErrorMessage = "Please enter the messages.")]
+        public List<MessageModel> Messages { get; set; }
+
+        public ChatModel(int id, List<string> participants, List<MessageModel> messages)
+        {
+            Id = id;
+            Participants = participants;
+            Messages = messages;
+        }
+
+        public ChatModel(List<string> participants, List<MessageModel> messages)
+        {
+            Id = id;
+            id++;
+            Participants = participants;
+            Messages = messages;
+        }
+
+        public ChatModel(List<string> participants)
+            : this(participants, new List<MessageModel>())
+        {
+        }
+    }
+}
